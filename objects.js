c9d925b89a13125c413f6851e86af8561e1e3226
Ricky Liu <rorix12321@gmail.com>
2018-06-09 07:06:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {'artist': 'song'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
@@ -8,3 +9,14 @@ function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
   return playlist;
 }
+=======
+var playlist = {obj: "key"};
+
+function updatePlaylist(obj, key, value) {
+  return object.assign(obj, { [key]: value})
+}
+
+function removeFromPlaylist(obj, key);
+  delete playlist.obj;
+  return playlist;
+>>>>>>> bee4d9881d819ab4f380fd219e8d6437decb09ae
